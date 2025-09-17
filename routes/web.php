2f273a23768b65f185601836fa63a272a9020461
Nqobile <ngobese.nn@gmail.com>
2025-09-17 14:07:26 +0200

    Done with Privacy

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::inertia('/music','Music')->name('page.music');
 Route::inertia('/videos','Video')->name('page.video');
 route::inertia('/news', 'News')->name('page.news');
 route::inertia('/terms', 'Terms')->name('page.terms');
+route::inertia('/privacy', 'Privacy')->name('page.privacy');
