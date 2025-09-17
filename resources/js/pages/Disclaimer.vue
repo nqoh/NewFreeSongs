28a9bbbf66cf3520df58694e68d8babcb9a67bbd
Nqobile <ngobese.nn@gmail.com>
2025-09-17 14:12:44 +0200

    Done with Disclaimer

---
+++ resources/js/pages/Disclaimer.vue
@@ -0,0 +1,38 @@
+<template>
+<section class="mbr-section" id="testimonials4-45" style="background-color: rgb(255, 255, 255); padding-top: 120px; padding-bottom: 120px;">
+
+<div class="mbr-section mbr-section__container mbr-section__container--middle">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 text-xs-center">
+                <h3 class="mbr-section-title display-2">Disclaimer</h3>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="mbr-section mbr-section-nopadding">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xs-12">
+
+                <div class="mbr-testimonial card">
+                    <div class="card-block"><p>All the Audio &amp; Video content found on the NewFreeSongs is not hosted on our servers nor is created or uploaded by us.</p><p><br></p><p>NewFreeSongs simply acts as a search engine that finds videos from websites like YouTube, DailyMotion, Metacafe and other video portals.</p><p><br></p><p>NewFreeSongs is not responsible for external website’s content.</p><p><br></p><p>If you find that some of the content violates your rights you may request for that content to be brought down by contacting us at info@NewFreeSongs.com</p><p><br></p><p>That will ensure that the content is removed from NewFreeSongs and other search engines that may have indexed the content.</p><p><br></p></div>
+                    <div class="mbr-author card-footer">
+
+                        <div class="mbr-author-name">For more information reach out at : info@newfreesongs.com</div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
+</section>
+    <Head title="Disclaimer |" />
+</template>
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::inertia('/videos','Video')->name('page.video');
 route::inertia('/news', 'News')->name('page.news');
 route::inertia('/terms', 'Terms')->name('page.terms');
 route::inertia('/privacy', 'Privacy')->name('page.privacy');
+route::inertia('/disclaimer', 'Disclaimer')->name('page.disclaimer');
