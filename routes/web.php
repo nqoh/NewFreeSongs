62defbd238e012c4197078a7d0a6bad8693144eb
Nqobile <ngobese.nn@gmail.com>
2025-09-17 13:39:36 +0200

    Done adding video page

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/','Welcome')->name('pages.welcome');
 Route::inertia('/music','Music')->name('page.music');
+Route::inertia('/videos','Video')->name('page.video');
