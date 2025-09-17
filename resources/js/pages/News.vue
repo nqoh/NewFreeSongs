1e1d393d6ac5f075dd52341d3a8ff6612a5060fe
Nqobile <ngobese.nn@gmail.com>
2025-09-17 13:56:42 +0200

    Done with news page

---
+++ resources/js/pages/News.vue
@@ -0,0 +1,27 @@
+<template>
+    <search />
+
+    <section class="mbr-cards mbr-section mbr-section-nopadding" id="features3-1q">
+     <div class="mbr-cards-row row" v-for="i in 3" :key="i">
+       <!----Music Card Component-->
+       <card title="HEAVY-K ft Mbuso Khoza - Thatha" type="music" description="After Heavy-K aka Drumboss Played his one hour set on LockDownHouseparty hosted by Channel O on DStv. He released a song, that will be on the next EP (KHUSTAEP ). The song is Called Thatha fearing Mbuso Khoza." 
+        btnClass="btn-success" image="test.jpg" action="Read More" v-for="i in 3" :key="i" />
+     </div>
+      <pagination />
+   </section>
+
+    <sharePage />
+
+    <Head title="News |" />
+</template>
+
+<script setup lang="ts">
+import search from      '@/components/app/Search.vue'
+import pagination from '@/components/app/pagination.vue';
+import sharePage from '@/components/app/sharePage.vue';
+import card from '@/components/app/card.vue';
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/','Welcome')->name('pages.welcome');
+Route::inertia('/','Welcome')->name('page.welcome');
 Route::inertia('/music','Music')->name('page.music');
 Route::inertia('/videos','Video')->name('page.video');
+route::inertia('/news', 'News')->name('page.news');
