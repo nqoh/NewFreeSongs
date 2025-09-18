81dce7038abfad9c645e9999e240a527c54f6450
Nqobile <ngobese.nn@gmail.com>
2025-09-18 17:23:52 +0200

    Done with 404 page and i deleted some unsused images

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+<section class="mbr-section">
+<div class="mbr-section mbr-section__container mbr-section__container--middle">
+    <div class="container">
+        <div class="row">
+          <div class="col-xs-12 text-xs-center">
+           <h3 class="mbr-section-title display-2" style="color: rgb(83, 83, 83);">Error</h3>
+           <small class="mbr-section-subtitle">404: Ooops NewFreeSongs not found...</small>
+           <img src="/assets/images/log.png"  />
+           </div>
+        </div>
+    </div>
+</div>
+</section>
+<Head title="NotFound |" />
+</template>
