951c668d0fe0781c8cc35d2b5f81e72178055795
Nqobile <ngobese.nn@gmail.com>
2025-09-26 22:58:06 +0200

    Done with resources i created all resources

---
+++ app/Http/Resources/NewsResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class newsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user_id' => $this->id,
+            'title'   => $this->title,
+            'description' => $this->description,
+            'image' => $this->image,
+            'comments' => $this->commentsResource()
+        ];
+    }
+}
---
+++ app/Http/Resources/VideoResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class videoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user_id' => $this->id,
+            'title'   => $this->title,
+            'description' => $this->description,
+            'image' => $this->image,
+            'endpoint' => $this->endpoint,
+        ];
+    }
+}
